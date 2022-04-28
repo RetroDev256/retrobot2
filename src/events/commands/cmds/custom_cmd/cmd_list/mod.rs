6a961f0b1a852ce5bcc8c7994160abac98ae5302
970037201 <ericpetersen3.14@gmail.com>
2022-04-28 20:43:30 +0100

    Bugfix

--- src/events/commands/cmds/custom_cmd/cmd_list/mod.rs
+++ src/events/commands/cmds/custom_cmd/cmd_list/mod.rs
@@ -17,6 +17,7 @@ pub async fn cmd_list(
         resp.interaction_response_data(|data| data.content("Listing commands for the server:"))
     })
     .await?;
+    let mut cmds_to_list = vec![];
     let response_opt = match CUST_CMDS.read() {
         Ok(cmds) => match int.guild_id {
             Some(guild_id) => match cmds.get(guild_id.as_u64()) {
@@ -24,22 +25,21 @@ pub async fn cmd_list(
                     true => Some("There are no commands for this server."),
                     _ => {
                         for (i, cmd) in cmd_list.iter().enumerate() {
-                            int.create_followup_message(&ctx.http, |followup| {
-                                let mut message = MessageBuilder::new();
-                                let fmt_cmd = message
-                                    .push_bold("Command ")
-                                    .push_bold(i)
-                                    .push_bold_line(':')
-                                    .push_bold_line("Regex key:")
-                                    .push_line(&cmd.regex)
-                                    .push_bold_line("Response:")
-                                    .push(&cmd.response)
-                                    .build();
-                                match fmt_cmd.len() >= 2000 {
-                                    true => followup.content(format!("{}...", &fmt_cmd[0..1997])),
-                                    _ => followup.content(fmt_cmd),
-                                }
-                            }).await?;
+                            let mut message = MessageBuilder::new();
+                            let fmt_cmd = message
+                                .push_bold("Command ")
+                                .push_bold(i)
+                                .push_bold_line(':')
+                                .push_bold_line("Regex key:")
+                                .push_line(&cmd.regex)
+                                .push_bold_line("Response:")
+                                .push(&cmd.response)
+                                .build();
+                            let msg_content = match fmt_cmd.len() >= 2000 {
+                                true => format!("{}...", &fmt_cmd[0..1997]),
+                                _ => fmt_cmd,
+                            };
+                            cmds_to_list.push(msg_content);
                         }
                         None
                     }
@@ -50,6 +50,10 @@ pub async fn cmd_list(
         },
         _ => Some("Unable to get a read lock on the command list."),
     };
+    for cmd_txt in cmds_to_list {
+        int.create_followup_message(&ctx.http, |followup| followup.content(cmd_txt))
+            .await?;
+    }
     if let Some(response) = response_opt {
         int.create_followup_message(&ctx.http, |followup| followup.content(response))
             .await?;
