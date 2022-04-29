a2f722575e21aa8e104c2082dc9139e02a8c7b93
970037201 <ericpetersen3.14@gmail.com>
2022-04-29 10:51:32 +0100

    o/

--- src/events/greets/mod.rs
+++ src/events/greets/mod.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 
 pub async fn greet(ctx: Context, new_member: &Member) -> Result<(), Box<dyn Error>> {
     let name = new_member.display_name();
-    let greet = format!("Welcome to the server {}!", name);
+    let greet = format!("Welcome to the server {}! o/", name);
     new_member
         .user
         .direct_message(ctx.http, |msg| msg.content(greet))
