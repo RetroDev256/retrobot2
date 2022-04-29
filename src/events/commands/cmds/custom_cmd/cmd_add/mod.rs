a3f050b90175457227510d43c40ac39c1d9dbe95
970037201 <ericpetersen3.14@gmail.com>
2022-04-29 06:36:24 +0100

    Regex add fix

--- src/events/commands/cmds/custom_cmd/cmd_add/mod.rs
+++ src/events/commands/cmds/custom_cmd/cmd_add/mod.rs
@@ -49,29 +49,42 @@ fn try_add_cmd<'a>(
     regex_option: Option<String>,
     reply_option: Option<String>,
     guild_id: Option<GuildId>,
-) -> &'a str {
+) -> String {
     match (regex_option, reply_option) {
-        (Some(regex_str), Some(reply)) => match RegexBuilder::new(&regex_str).build() {
-            Ok(_regex) => match CUST_CMDS.write() {
-                Ok(mut lock) => match guild_id {
-                    Some(server_id) => match lock.get_mut(&server_id.0) {
-                        Some(cmds) => match cmds.insert(regex_str, reply) {
-                            None => "Successfully added new command.",
-                            Some(_) => "Overwrote old command with the same key.",
+        (Some(regex_str), Some(reply)) => {
+            match RegexBuilder::new(&regex_str).size_limit(1048576).build() {
+                Ok(_regex) => match CUST_CMDS.write() {
+                    Ok(mut lock) => match guild_id {
+                        Some(server_id) => match lock.get_mut(&server_id.0) {
+                            Some(cmds) => match cmds.insert(regex_str, reply) {
+                                None => "Successfully added new command.".to_owned(),
+                                Some(_) => "Overwrote old command with the same key.".to_owned(),
+                            },
+                            _ => {
+                                let _ =
+                                    lock.insert(server_id.0, HashMap::from([(regex_str, reply)]));
+                                "Successfully added first command.".to_owned()
+                            }
                         },
-                        _ => {
-                            let _ = lock.insert(server_id.0, HashMap::from([(regex_str, reply)]));
-                            "Successfully added first command."
-                        }
+                        _ => "This is not a server.".to_owned(),
                     },
-                    _ => "This is not a server.",
+                    _ => "Unable to obtain a write lock on server commands list.".to_owned(),
                 },
-                _ => "Unable to obtain a write lock on server commands list.",
-            },
-            _ => "Invalid regex.",
-        },
-        (Some(_regex_str), None) => "Response was not provided.",
-        (None, Some(_reply)) => "Regex was not provided.",
-        _ => "Neither regex nor response was provided.",
+                Err(regex_err) => match regex_err {
+                    regex::Error::Syntax(syntax) => match syntax.len() > 1972 {
+                        true => format!("Invalid regex syntax: ```{}...```", &syntax[..1969]),
+                        false => format!("Invalid regex syntax: ```{}```", syntax),
+                    },
+                    regex::Error::CompiledTooBig(too_big) => format!(
+                        "Compiled regex exceeded allowed 1 MiB: {} bytes used.",
+                        too_big
+                    ),
+                    _ => "Failed to compile regex.".to_owned(),
+                },
+            }
+        }
+        (Some(_regex_str), None) => "Response was not provided.".to_owned(),
+        (None, Some(_reply)) => "Regex was not provided.".to_owned(),
+        _ => "Neither regex nor response was provided.".to_owned(),
     }
 }
