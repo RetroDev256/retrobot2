7595d462b2161cf205877644bc7d5e03a3b019ea
970037201 <ericpetersen3.14@gmail.com>
2022-05-02 14:37:54 +0100

    Better progress

--- src/events/commands/cmds/msg_digest/mod.rs
+++ src/events/commands/cmds/msg_digest/mod.rs
@@ -28,8 +28,8 @@ pub async fn msg_digest(int: ApplicationCommandInteraction, ctx: Context) {
         _ => None,
     }
     .unwrap();
-    let range_err = match bytes <= 0 || bytes > 8388608 {
-        true => Some("The number of bytes in the digest must be between 1 or 8388608 bytes."),
+    let range_err = match bytes <= 0 || bytes > 4194304 {
+        true => Some("The number of bytes in the digest must be between 1 or 4194304 bytes."),
         false => match rounds <= 0 || rounds > 256 {
             true => Some("The number of rounds must be between 1 and 256."),
             false => None,
@@ -45,23 +45,34 @@ pub async fn msg_digest(int: ApplicationCommandInteraction, ctx: Context) {
         }
         _ => {
             let file = attachment.download().await.unwrap();
+            int.create_interaction_response(&ctx.http, |resp| {
+                resp.interaction_response_data(|data| {
+                    data.content("Hashing blocks in file and combining them into digest...")
+                })
+            }).await.unwrap();
             let digest = arb_digest(&file, bytes as usize, rounds as u64);
+            int.create_followup_message(&ctx.http, |data| {
+                data.content("Converting to hexadecimal...")
+            }).await.unwrap();
             let hex_bytes: String = digest
                 .into_iter()
                 .map(|byte| format!("{:02X}", byte))
                 .collect();
             if bytes <= 997 {
                 let message = format!("```{}```", hex_bytes);
-                int.create_interaction_response(&ctx.http, |resp| {
-                    resp.interaction_response_data(|data| data.content(message))
+                int.create_followup_message(&ctx.http, |data| {
+                    data.content(message)
                 })
                 .await
                 .unwrap();
             } else {
                 let mut tmp_content = Builder::new().suffix(".txt").tempfile().unwrap();
+                int.create_followup_message(&ctx.http, |data| {
+                    data.content("Writing to file...")
+                }).await.unwrap();
                 write!(tmp_content, "{}", hex_bytes).unwrap();
-                int.create_interaction_response(&ctx.http, |resp| {
-                    resp.interaction_response_data(|data| data.add_file(tmp_content.path()))
+                int.create_followup_message(&ctx.http, |data| {
+                    data.add_file(tmp_content.path())
                 })
                 .await
                 .unwrap();
