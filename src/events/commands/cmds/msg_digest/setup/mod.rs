3636d4c5e77a0cba8c16c74b49900a1b32c0643d
970037201 <ericpetersen3.14@gmail.com>
2022-05-05 21:11:40 +0100

    fix bug

--- src/events/commands/cmds/msg_digest/setup/mod.rs
+++ src/events/commands/cmds/msg_digest/setup/mod.rs
@@ -5,7 +5,7 @@ use serenity::{
 
 pub fn msg_digest_setup(cmds: &mut CreateApplicationCommands) {
     cmds.create_application_command(|cmd| {
-        cmd.name("digest")
+        cmd.name("msg_digest")
             .description("512 bit, 2 round arb_digest digest of a string")
             .create_option(|opt| {
                 opt.name("input")
